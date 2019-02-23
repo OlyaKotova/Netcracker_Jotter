34583c20f615eac92dc006cff89677198826b127
Olya Kotova <sanctim@mail.ru>
2019-02-23 21:02:35 +0300

    #3

--- src/app/home-window/cart-window/cart-window.component.ts
+++ src/app/home-window/cart-window/cart-window.component.ts
@@ -22,6 +22,8 @@ export class CartWindowComponent implements OnInit {
   index: number;
   newList: number[] = [];
 
+  arr2: number[] = [];
+
   constructor(
     private userService: UserService,
     private cookieService: CookieService,
@@ -35,7 +37,7 @@ export class CartWindowComponent implements OnInit {
           this.subTotal += 20.50;
           this.amt1 += 1;
         }
-        if (!((this.currCart).indexOf(2) === -1)) {
+        if (this.currCart[i] === 2) {
           this.subTotal += 19.50;
           this.amt2 += 1;
         }
@@ -54,13 +56,8 @@ export class CartWindowComponent implements OnInit {
   }
   delete(quantity) {
     this.index = (this.currCart).indexOf(quantity);
-    console.log(this.index);
-    (this.currCart).splice(quantity, 1);
-    this.currentUser = JSON.parse(this.cookieService.get('currentUser'));
-    console.log((this.currentUser)['cart']);
-    //console.log('111  ' + typeof(this.currentUser['cart']));
-    //this.currentUser['cart'] = this.currCart;
-    this.cookieService.get('currentUser');
+    (this.currentUser['cart']).splice(this.index, 1);
+    this.cookieService.set('currentUser', JSON.stringify(this.currentUser));
+    location.reload();
   }
-
 }
